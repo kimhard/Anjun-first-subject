f2df1c0750316969dcef1111ddafc416f50bad2d
이창현 <rchea0123@gmail.com>
2022-12-22 17:19:53 +0900

    flask.jsp_이창현

--- Anjun_Plus/src/main/java/Controller/test.java
+++ Anjun_Plus/src/main/java/Controller/test.java
@@ -11,12 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 public class test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		
-		String sql = "INSERT INTO anjun_user (user_id, user_pw, user_nick, user_name, user_email, user_rrn) "
-				+ "VALUES (?, ?, ?, ?, ?, ?)";
 		
-		System.out.println(sql);
 	}
 
 }
