1589f47daed779302e3e873699e003ad512a62de
smhrd <smhrd@125.136.57.56>
2022-12-23 12:40:57 +0900

    SQL문 수정_이영미

--- Anjun_Plus/src/main/java/Model/Comment_reportDAO.java
+++ Anjun_Plus/src/main/java/Model/Comment_reportDAO.java
@@ -48,12 +48,11 @@ public class Comment_reportDAO {
 	public int reportPost(Comment_ReportDTO dto) {
 			try {
 				getConn();
-				String sql = "INSERT INTO anjun_post_report(cr_seq, cmt_seq,cr_reason,user_id) VALUES (?, ?, ?, ?)";
+				String sql = "INSERT INTO anjun_comment_report(cmt_seq, cr_reason, user_id) VALUES (?, ?, ?)";
 				psmt = conn.prepareStatement(sql);
-				psmt.setInt(1, dto.getCr_seq());
-				psmt.setInt(2, dto.getCmt_seq());
-				psmt.setString(3, dto.getCr_reason());
-				psmt.setString(4, dto.getUser_id());
+				psmt.setInt(1, dto.getCmt_seq());
+				psmt.setString(2, dto.getCr_reason());
+				psmt.setString(3, dto.getUser_id());
 				cnt = psmt.executeUpdate();
 			} catch (Exception e) {
 				e.printStackTrace();
