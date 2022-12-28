c079bbe24a563b4b99f35756b84570ebe89786f6
나건영 <renmsts@naver.com>
2022-12-28 16:23:45 +0900

    로그인 css추가_ 나건영

--- Anjun_Plus/src/main/java/Controller/JoinService.java
+++ Anjun_Plus/src/main/java/Controller/JoinService.java
@@ -13,35 +13,36 @@ import Model.UserDTO;
 @WebServlet("/JoinService")
 public class JoinService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		request.setCharacterEncoding("UTF-8");
-		
+
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		String nick = request.getParameter("nick");
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
-		String rrn = request.getParameter("rrn");
-		
+		String rrn1 = request.getParameter("rrn1");
+		String rrn2 = request.getParameter("rrn2");
+		String rrn = rrn1 + "-" + rrn2;
+
 		UserDTO dto = new UserDTO(id, pw, nick, name, email, rrn, "C");
 		UserDAO dao = new UserDAO();
-		
+
 		System.out.println(dto.getId());
 		System.out.println(dto.getPw());
 		System.out.println(dto.getNick());
 		System.out.println(dto.getName());
 		System.out.println(dto.getEmail());
 		System.out.println(dto.getRrn());
-		
+
 		int cnt = dao.join(dto);
-		if (cnt > 0) {
-			System.out.println("회원가입이 완료되었습니다.");
-		}else {
-			System.out.println("회원가입 실패.");
-		}
-		response.sendRedirect("join.jsp");
-		
+
+		response.setStatus(cnt);
+		response.sendRedirect("Login.jsp");
+
 	}
 
 }
