a9b9a361318fce36301f61d9a839af59bdb25f3c
이창현 <rchea0123@gmail.com>
2022-12-30 09:46:03 +0900

    Alarm, FileDAO, FileDTO, UserStamp_이창현

---
+++ Anjun_Plus/src/main/java/Model/FileDAO.java
@@ -0,0 +1,58 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class FileDAO {
+
+	// DB 연결용 객체 생성
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs = null;
+	int cnt = 0;
+
+	// DB 연결용 getConn()
+	public void getConn() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String dbid = "cgi_4_1220_3";
+			String dbpw = "smhrd3";
+			conn = DriverManager.getConnection(url, dbid, dbpw);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// DB 연결용 객체 반환 close()
+	public void close() {
+		try {
+			if(rs != null) {
+				rs.close();
+			}
+			if(psmt != null) {
+				psmt.close();
+			}
+			if(conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 글에 표시되는 이미지 불러오기
+	public void readPost(int post_seq) {
+		try {
+			getConn();
+			String sql = "SELECT ";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+	}
+}
